58a64b15503dbd9f7049e27dfbed81937ecb9e15
Pavel Demyanov <stokato90@gmail.com>
2026-03-10 01:12:06 +0300

    converter

---
+++ 1-converter/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	const EUR = 0.8607
+	const RUB = 79.15
+	var countRub = 1000.0
+	var countEur = 150.0
+	resultEur := EUR * (countRub / RUB)
+	resultRub := RUB * (countEur / EUR)
+	fmt.Printf("Конвертация из RUB в EUR = %F\n", resultEur)
+	fmt.Printf("Конвертация из EUR в RUB = %F\n", resultRub)
+}
